0ce505c369839838fb0e68dd66d537eb36bc28d9
nuddik <ned@tekmini.nn.lab>
2017-02-25 20:23:43 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Apple Mail HTML Signature Installer
+
+A simple script written in Python to install HTML signatures in Apple Mail. There are two Python files, 'signature_installer.py' can be used in the command line, and 'signature_installer_automator' is the Python code used in the Automator app. There is also an Automator App included, this is simply an Automator app that asks for your HTML signature file and passes it to the Python script to install it.
+
+## Warning!
+
+Use at your own risk, it works fine on my system (Macos Sierra), but I'm very new to coding and I take no responsibility for anything that goes wrong on your system...
+
+## Using it
+
+First, you'll need an html file that you have created or been given. If you don't have this, you will need to figure that out first(Google...).
+
+Once you have your HTML signature ready you'll need to follow a couple steps to get it installed.
+
+Before you use the app, you need to create a blank signature in Apple Mail for the script to overwrite. To do this, open Apple Mail, go to Mail Preferences and then locate the 'Signatures' tab. Select the mail account on the left that you would like to add the signature to and then add a signature with the '+' button. After that, you don't need to do anything else, just leave the default information that it's populated with and make sure you 'Quit' Mail.
+
+Next, either use teh automator app or python script...
+
+To use the Automator app, simply double click the app and select your HTML signature file. That's it.
+
+On the command line, it can be used with the '-i' option to pass you HTML file to the script as follows
+```
+python ./signature_installer.py -i signature_file.html
+```
+
+## Contributing
+
+If anyone would like to improve it please go ahead, I'm not much of a coding guru...
+
+Even better, if anyone has any idea how to use Kivy to make the app as opposed to Automator that would be great. I couldn't figure out how to get it going at all...
+
+
+## License
+
+This project is licensed under the MIT License.
+
+## Acknowledgments
+
+Lot's of Googling and tutorials have all been melded to get to where we are now.
+
+## Issues or Suggestions
+
+Feel free to submit an issues on GitHub if you have any trouble, or any suggestions.
---
+++ sig_installer_automator.py
@@ -0,0 +1,60 @@
+import os
+import glob
+from os.path import expanduser
+import sys
+import time
+
+# Get input file
+for f in sys.argv[1:]:
+    inputfile = f
+
+    # Get users home folder
+    home = expanduser("~")
+
+    # Test to see which signature folder to use, depending on OS version
+    if os.path.exists('{}/Library/Mail/V4/MailData/Signatures/'.format(home)):
+        signatures_folder = ('/Library/Mail/V4/MailData/Signatures/')
+    elif os.path.exists('{}/Library/Mail/V3/MailData/Signatures/'.format(home)):
+        signatures_folder = ('/Library/Mail/V3/MailData/Signatures/')
+    elif os.path.exists('{}/Library/Mail/V2/MailData/Signatures/'.format(home)):
+        signatures_folder = ('/Library/Mail/V2/MailData/Signatures/')
+    else:
+        print("No Mail Signature folder found! Something is wrong...")
+        sys.exit(2)
+
+    #Get list of files in 'Signature folder'
+    list_of_files = glob.glob('{}{}*.mailsignature'.format(home, signatures_folder))
+
+    # Get last edited file in signatures folder
+    latest_signature = max(list_of_files, key=os.path.getctime)
+    print(latest_signature)
+
+    # Get html signature from input file
+    f = open(inputfile, 'r')
+    html_signature = f.read()
+    f.close()
+
+    # Create backup of current mail signature
+    f = open(latest_signature, 'r')
+    current_signature = f.read()
+    f.close()
+    f = open('{}.{}.old'.format(latest_signature, time.strftime("%Y.%m.%d.%H%M%S")), 'w')
+    f.write(current_signature)
+    f.close()
+
+    # Read first 5 lines of current signature from mail directory
+    with open(latest_signature) as myfile:
+        new_signature = [next(myfile) for x in xrange(5)]
+    new_signature = ''.join(map(str, new_signature))  # Create string from list of first 5 lines read from file
+    new_signature = new_signature + '\n' + html_signature  # Append html for first 5 lines of signature
+    print(new_signature)
+
+    # Unlock current signature incase it's locked from a previous edit...
+    os.system("chflags nouchg {}".format(latest_signature))
+
+    # Write new signature to disk, this will overwrite the current one.
+    wr = open(latest_signature, 'w')
+    wr.write(new_signature)
+    wr.close()
+
+    os.system("chflags uchg {}".format(latest_signature))
